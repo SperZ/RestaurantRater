899731c8808221a51d72e90745ed54e65a2dbc7f
SperZ <ZachSperka@gmail.com>
2020-08-17 11:16:33 -0400

    Setup Restaurant Index View

--- RestaurantRater/Models/Restaurant.cs
+++ RestaurantRater/Models/Restaurant.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
@@ -12,7 +13,7 @@ namespace RestaurantRater.Models
         public int RestaurantID { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
-        public int rating { get; set; }
+        public int Rating { get; set; }
     }
 
     public class RestaurantDbContext : DbContext 
