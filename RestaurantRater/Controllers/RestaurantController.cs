afb9e683d6eb97617670c5d83d7b5676afc09baa
SperZ <ZachSperka@gmail.com>
2020-08-17 10:35:56 -0400

    Added Restuarant Controller and Basic index view

---
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantRater.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        // GET: Restaurant/Index Method, this index method returns the view to UI
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
