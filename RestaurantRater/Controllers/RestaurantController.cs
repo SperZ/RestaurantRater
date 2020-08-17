9c13c5f43ccfd16f70bf8af88b1211636560683e
SperZ <ZachSperka@gmail.com>
2020-08-17 15:01:57 -0400

    finished product for now

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -2,6 +2,7 @@
 using RestaurantRater.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -63,6 +64,51 @@ namespace RestaurantRater.Controllers
             _dbContext.Restaurants.Remove(restaurant);
             _dbContext.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        // GET: Restaurant/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _dbContext.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Entry(restaurant).State = EntityState.Modified;
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurant);
+        }
+
+        //Get: Restaurant/Details{id}
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _dbContext.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
         }
     }
 }
--- RestaurantRater/Models/Restaurant.cs
+++ RestaurantRater/Models/Restaurant.cs
@@ -7,13 +7,19 @@ using System.Web;
 
 namespace RestaurantRater.Models
 {
+    public enum TypeOfCuisine {Thai=1, Mexican, American, Chinese, Sushi, Italian, German, Polish, French, Jamiacan, Ethopian, Greek, English, Indian }
     public class Restaurant
     {
         [Key]
         public int RestaurantID { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Address { get; set; }
+        [Required]
+        [Range(1,5)]
         public int Rating { get; set; }
+
+        public TypeOfCuisine CuisineType { get; set; }
     }
 
     public class RestaurantDbContext : DbContext 
